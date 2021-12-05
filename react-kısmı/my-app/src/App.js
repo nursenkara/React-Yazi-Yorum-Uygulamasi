be1922c91e26e44358e8a33a5e2e21da82bc4f33
Nur SENKARA <nsenkara19@gmail.com>
2021-12-05 22:15:35 +0300

    API'den get metoduyla yorumlar çekildi.Console da görüntülendi.

--- react-kısmı/my-app/src/App.js
+++ react-kısmı/my-app/src/App.js
@@ -1,11 +1,28 @@
-
-
+import React, { useEffect ,useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [yaziListesi,setYaziListesi]= useState([])
+  useEffect(() => {
+    axios
+      .get("https://react-yazi-yorum.herokuapp.com/posts")
+      .then((response) => {
+        setYaziListesi(response.data);
+      });
+  }, []);
   return (
-  <div>
-    
-  </div>
+    <div className="main_wrapper">
+      <header></header>
+      <div class="ui raised very padded text container segment">
+        <div className="item">
+          <i className="large github middle aligned icon"></i>
+          <div className="content">
+            <span className="header">Semantic-Org/Semantic-UI</span>
+            <div className="description">Updated 10 mins ago</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
